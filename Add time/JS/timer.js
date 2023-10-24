bb4472dadc88848d1b99b28a23e08323feb72922
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-10-24 13:12:38 +0330

    Add default timer(showTime()).

--- Add time/JS/timer.js
+++ Add time/JS/timer.js
@@ -1,39 +1,61 @@
 // select timer box.
 const timerBox = document.querySelector("#timer-box");
+const timerMinutes = document.querySelector("#timer-minutes");
 
-for (let i = 12; i > 0; i--) {
-  i = i < 10 ? "0" + i : i;
-  let option = `<option value="${i}">${i}</option>`;
-  timerBox[0].firstElementChild.insertAdjacentHTML("afterend", option);
-}
-
-const config = {
-  timeSet: {
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  },
-};
-
-function diffrence(time) {
-  time = config.timeSet.minutes + time;
-  return time;
-}
-
-function displayTimer() {
-  //   create option for set timer.
-  const optionBox = document.createElement("option");
-  // append option to the timer box.
-  timerBox.appendChild(optionBox);
-  //   start timer:
-  const startTime = 0;
-  const endTime = 24;
-  //   create a (for) loop for making option box.
-  for (let i = startTime; i <= endTime; i += diffrence(20)) {
-    timerBox.innerHTML += `<option> ${i} <option/>`;
+function showTime() {
+  for (let i = 23; i >= 0; i--) {
+    i = i < 10 ? "0" + i : i;
+    for (let j = 45; j >= 0; j -= 15) {
+      j = j < 10 ? "0" + j : j;
+      // create a template.
+      let option = `<option value="${i}">${i}:${j}</option>`;
+      timerBox.innerHTML += option;
+    }
   }
 }
 
-displayTimer();
+showTime();
 
-function currentTime() {}
+// for (let j = 60; j >= 0; j -= 15) {
+//   j = j < 10 ? "0" + j : j;
+//   let option = `<option value="${j}">${j}</option>`;
+//   // timerBox[1].firstElementChild.insertAdjacentHTML("afterend", option);
+//   timerMinutes.innerHTML += option;
+// }
+
+// for (let i = 2; i > 0; i--) {
+//   let ampm = i == 1 ? "AM" : "PM";
+//   let option = `<option value="${ampm}">${ampm}</option>`;
+//   timerBox[2].firstElementChild.insertAdjacentHTML("afterend", option);
+// }
+
+// const config = {
+//   timeSet: {
+//     hours: 0,
+//     minutes: 0,
+//     seconds: 0,
+//   },
+// };
+
+// function diffrence(time) {
+//   time = config.timeSet.minutes + time;
+//   return time;
+// }
+
+// function displayTimer() {
+//   //   create option for set timer.
+//   const optionBox = document.createElement("option");
+//   // append option to the timer box.
+//   timerBox.appendChild(optionBox);
+//   //   start timer:
+//   const startTime = 0;
+//   const endTime = 24;
+//   //   create a (for) loop for making option box.
+//   for (let i = startTime; i <= endTime; i += diffrence(20)) {
+//     timerBox.innerHTML += `<option> ${i} <option/>`;
+//   }
+// }
+
+// displayTimer();
+
+// function currentTime() {}
