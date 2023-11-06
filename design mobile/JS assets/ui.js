f02c3f10a8a5c49b29940ee7d5c3df879606addb
Mahsa Arabzadeh <arabzadehmah@gmail.com>
2023-11-06 18:39:35 +0330

    Add current date into the header.

--- design mobile/JS assets/ui.js
+++ design mobile/JS assets/ui.js
@@ -1,4 +1,6 @@
 const welcomeName = document.querySelector(".welcome-name");
+const header = document.querySelector("#header");
+const searchBox = document.querySelector(".search-box");
 
 function showWelcomeMessage() {
   const userName = prompt("لطفاً نام خود را وارد کنید:");
@@ -9,3 +11,31 @@ function showWelcomeMessage() {
   }
 }
 showWelcomeMessage();
+
+function getFormattedDate() {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const today = new Date();
+  const day = String(today.getDate()).padStart(2, "0");
+  const month = months[today.getMonth()];
+
+  const formattedDate = `Today ${month}, ${day}`;
+  const dateToday = document.createElement("div");
+  dateToday.classList.add("date-today");
+  dateToday.innerHTML = formattedDate;
+  header.insertBefore(dateToday, searchBox);
+  return formattedDate;
+}
+getFormattedDate();
